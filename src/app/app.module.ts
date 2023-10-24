a041d101664a2d66565acdfc8c3d5eb6ac9a39b1
miketaxy <miketaxy71@gmail.com>
2023-10-24 18:32:36 +0300

    transfer from js to angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstGameComponent } from './first-game/first-game.component';
+import {FirstGameService} from "./first-game/first-game.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstGameComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [FirstGameService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
