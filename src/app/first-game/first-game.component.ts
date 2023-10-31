9b5e9f9ab4efe7ccf6aa816d6bac796d05c878eb
miketaxy <miketaxy71@gmail.com>
2023-10-31 22:25:37 +0200

    rebasing

--- src/app/first-game/first-game.component.ts
+++ src/app/first-game/first-game.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit} from '@angular/core';
 import {FirstGameService} from "./first-game.service";
 
 @Component({
@@ -6,29 +6,59 @@ import {FirstGameService} from "./first-game.service";
   templateUrl: './first-game.component.html',
   styleUrls: ['./first-game.component.css']
 })
-export class FirstGameComponent implements OnInit {
-  constructor(protected firstGameService: FirstGameService) {
-  }
+export class FirstGameComponent implements OnInit, EventEmitter<boolean> {
+
+
+  private styleFigure: Map<string, string> = new Map<string, string>([['circle', 'none'], ['square', 'none'], ['triangle', 'none']]);
+  private isDrawn: boolean = false;
+  private isClicked: boolean = false;
+  private start: boolean = false;
+  private times = [];
+
 
   ngOnInit() {
-    //start game
     document.addEventListener('keypress', (event) => {
-      if (event.key == ' ' && !this.firstGameService.startGetter) {
-        this.firstGameService.startSetter = true;
-        this.firstGameService.startGame();
-      } else if (event.key == ' ' && this.firstGameService.startGetter) {
-        this.firstGameService.clickHandler(this.firstGameService.figureGetter);
+      if(!this.start && event.key == ' ') {
+        this.game();
+      }
+      else if(event.key == ' '){
+        this.isClicked = true;
       }
     });
   }
-  get styleFigureGetter(): Map<string, string> {
-    return this.firstGameService.styleFigureGetter;
-  }
-  hideAndShowElements(): string {
-    return this.firstGameService.hideAndShowElements();
+
+  game() {
+    let figure;
+    let gameCounter = 0;
+    while (gameCounter < 5)
+      this.setTimer().then(() => {
+        figure = this.drawFigure()
+      }).then
   }
 
-  calculate() {
-    return this.firstGameService.averageTimeGetter;
+  drawFigure() {
+    let figures = Array.from(this.styleFigure.keys())
+    let figure = figures[Math.floor(Math.random() * figures.length)];
+    this.styleFigure.set(figure, 'block');
+    this.isDrawn = true;
+    return figure;
+  }
+
+  removeFigure(figure: string) {
+    this.styleFigure.set(figure, 'none');
+  }
+
+
+  setTimer($event?: any): Promise<boolean> {
+      let randomTime = Math.floor(Math.random() * 3000) + 1000;
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(true);
+        }, randomTime);
+      })
+    }
+
+  clickHandler() {
+
   }
 }
